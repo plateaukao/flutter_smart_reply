f09d73e0538fc210557d71a71863b40696d83d1a
Daniel Kao <daniel.kao@gmail.com>
2020-08-14 01:04:16 +0800

    update export file

--- lib/flutter_smart_reply.dart
+++ lib/flutter_smart_reply.dart
@@ -1,34 +1,3 @@
 
-import 'dart:async';
-import 'dart:convert';
 
-import 'package:flutter/services.dart';
-
-class FlutterSmartReply {
-  static const MethodChannel _channel = const MethodChannel('flutter_smart_reply');
-
-  static Future<List<String>> getSmartReplies(List<TextMessage> messageList) async {
-    final jsonString = await _channel.invokeMethod('getSmartReplies', jsonEncode(messageList));
-    return List<String>.from(jsonDecode(jsonString));
-  }
-}
-
-class TextMessage {
-  String text;
-  int timestamp;
-  bool isSelf;
-
-
-  TextMessage(this.text, this.timestamp, this.isSelf);
-
-  Map<String, dynamic> toJson() =>
-      {
-        'text': text,
-        'timestamp': timestamp,
-        'isSelf' : false,
-      };
-
-  TextMessage.createForLocalUser(this.text, this.timestamp) : this.isSelf= true;
-
-  TextMessage.createForRemoteUser(this.text, this.timestamp) : this.isSelf = false;
-}
+export 'src/smart_reply.dart';
