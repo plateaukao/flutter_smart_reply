3140c45481f5f335f1043a099b976caf820196ec
Daniel Kao <daniel.kao@linecorp.com>
2020-08-14 20:17:52 +0800

    add doc

--- lib/src/smart_reply.dart
+++ lib/src/smart_reply.dart
@@ -3,15 +3,18 @@ import 'dart:convert';
 
 import 'package:flutter/services.dart';
 
+/// A Class to interact with MLKit Smart Reply engine.
 class FlutterSmartReply {
   static const MethodChannel _channel = const MethodChannel('flutter_smart_reply');
 
+  /// Get suggested reply string list based on input message string list
   static Future<List<String>> getSmartReplies(List<TextMessage> messageList) async {
     final jsonString = await _channel.invokeMethod('getSmartReplies', jsonEncode(messageList));
     return List<String>.from(jsonDecode(jsonString));
   }
 }
 
+/// A data class to hold content for each text message
 class TextMessage {
   String text;
   int timestamp;
