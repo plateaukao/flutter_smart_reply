d862d2d5fd65071851d0474fe3e8148564558095
Daniel Kao <daniel.kao@linecorp.com>
2020-08-14 19:51:17 +0800

    try to make iOS work too. not working yet

--- ios/Classes/SwiftFlutterSmartReplyPlugin.swift
+++ ios/Classes/SwiftFlutterSmartReplyPlugin.swift
@@ -1,7 +1,10 @@
 import Flutter
 import UIKit
+import MLKit
 
 public class SwiftFlutterSmartReplyPlugin: NSObject, FlutterPlugin {
+  lazy var smartReply = SmartReply.smartReply()
+    
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "flutter_smart_reply", binaryMessenger: registrar.messenger())
     let instance = SwiftFlutterSmartReplyPlugin()
@@ -9,6 +12,28 @@ public class SwiftFlutterSmartReplyPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    
+    var conversation: [TextMessage] = []
+
+    // Then, for each message sent and received:
+    let message = TextMessage(
+        text: "How are you?",
+        timestamp: Date().timeIntervalSince1970,
+        userID: "userId",
+        isLocalUser: false)
+    conversation.append(message)
+    
+    SmartReply.smartReply().suggestReplies(for: conversation) { result, error in
+        guard error == nil, let result = result else {
+            return
+        }
+        if (result.status == .notSupportedLanguage) {
+            // The conversation's language isn't supported, so
+            // the result doesn't contain any suggestions.
+        } else if (result.status == .success) {
+            // Successfully suggested smart replies.
+            // ...
+        }
+    }
   }
 }
--- test/flutter_smart_reply_test.dart
+++ test/flutter_smart_reply_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_smart_reply/flutter_smart_reply.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('flutter_smart_reply');
@@ -15,9 +14,5 @@ void main() {
 
   tearDown(() {
     channel.setMockMethodCallHandler(null);
-  });
-
-  test('getPlatformVersion', () async {
-    expect(await FlutterSmartReply.platformVersion, '42');
   });
 }
